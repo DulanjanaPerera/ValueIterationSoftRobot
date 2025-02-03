f7ab66c5810808954104c2c21644394cf7b8f49b
Dulanjana Perera <83073310+DulanjanaPerera@users.noreply.github.com>
2025-02-02 22:45:22 -0600

    Update main_VI.py
    add comments

--- main_VI.py
+++ main_VI.py
@@ -16,16 +16,26 @@ file_pathP = os.path.join(current_directory, "Data", "policy_2step.json")
 
 # render the simulation
 gui = True
+
+# create simulation object
 tetra = TetraSim(gui, 190)
 tetra.run()
+
+# create rolling gait object
 gait = Rolling()
 gait.half_circle_coordinates(0.09)
 
+# robot's start location
 start = [0.0, 0.0, 2]
+
+# robot's goal location
 goal = [1.2, 0.0, 1]
 
+# create value iteration object with parameters
 vi = ValueIteration(tetra, gait, 2, start, goal)
 start_time = time.time()
+
+# run the iterations
 vi.vi()
 end_time = time.time()
 
